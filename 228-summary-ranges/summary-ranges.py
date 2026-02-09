34b5ee6bdc5a65fc3422d9b948df9abd8ee343a8
Ammu-ai-ai <sahooamitra1@gmail.com>
2026-02-09 13:27:47 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (59.85%) - LeetSync

---
+++ 228-summary-ranges/summary-ranges.py
@@ -0,0 +1,24 @@
+class Solution:
+    def summaryRanges(self, nums):
+        res = []
+        n = len(nums)
+        i = 0
+
+        while i < n:
+            start = nums[i]
+
+            while i + 1 < n and nums[i + 1] == nums[i] + 1:
+                i += 1
+
+            end = nums[i]
+
+            if start == end:
+                res.append(str(start))
+            else:
+                res.append(str(start) + "->" + str(end))
+
+            i += 1
+
+        return res
+
+
