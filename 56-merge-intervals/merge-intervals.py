97f1ce48ca580d9e239dab95f48d37836a5db83f
Ammu-ai-ai <sahooamitra1@gmail.com>
2026-03-31 22:17:11 +0530

    Time: 7 ms (97.16%) | Memory: 16.1 MB (48.56%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+    
+        intervals.sort()  # Sort by start time
+        merged = []
+        prev = intervals[0]
+
+        for i in range(1, len(intervals)):
+            if intervals[i][0] <= prev[1]:  # Overlap
+                prev[1] = max(prev[1], intervals[i][1])  # Merge
+            else:
+                merged.append(prev)
+                prev = intervals[i]
+
+        merged.append(prev)
+        return merged
